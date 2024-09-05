8247fa8a0a02a335a6f03620eeec75cf063109e1
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2024-09-05 21:56:43 +0530

    python vs bash

---
+++ first-script.py
@@ -0,0 +1,8 @@
+# python is a interpreted programming language
+print("Welcome to the first script")
+
+# Simple calculator for addition
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+result = num1 + num2
+print(f"The sum is: {result}")
---
+++ pythonVSbash.py
@@ -0,0 +1,25 @@
+# #!/bin/bash
+#
+# # Define a variable
+# age=18
+#
+# # Use if condition to check the value of the variable
+# if [ $age -ge 18 ]; then
+#     echo "You are eligible to vote."
+# else
+#     echo "You are not eligible to vote."
+# fi
+
+######################################################################
+# in below, that include the python code for above bash scripting code
+######################################################################
+
+# Define a variable
+age = 18
+
+# Use if condition to check the value of the variable
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
