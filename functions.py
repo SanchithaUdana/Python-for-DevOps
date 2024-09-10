ec6375878ee9c0cacfc3ee2b8f7f593bfc0bc1af
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2024-09-10 13:03:23 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,17 @@
+# defining a function
+
+def adding(var1, var2):
+    return var1 + var2
+
+
+answer = adding(12, 45)
+print(answer)
+
+
+# function 2
+def Adding(val1, val2):
+    total = val1 + val2
+    print(total)
+
+
+Adding(12, 45)
